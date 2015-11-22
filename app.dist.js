837b6d90025b96b4e8bf90871fb557cdf309db34
mel1313 <melvinleil@gmail.com>
2015-11-22 13:34:14 +0800

    stickgame

---
+++ app.dist.js
@@ -0,0 +1,15 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+"use strict";
+
+var player1;
+var player2;
+var gameStatus; // Display's players turn and show winner
+var undoButton; // Button to undo move
+var confirmButton; // Button to confirm player's move
+var clearStick;
+
+var gameBoard = [["row1stick1"], ["row2stick1", "row2stick2"], ["row3stick1", "row3stick2", "row3stick3"]];
+
+// player 1 to start the game
+
+},{}]},{},[1]);
---
+++ app.js
@@ -0,0 +1,13 @@
+
+var player1
+var player2
+var gameStatus // Display's players turn and show winner
+var undoButton // Button to undo move
+var confirmButton // Button to confirm player's move
+var clearStick
+
+var gameBoard = [["row1stick1"],
+                ["row2stick1", "row2stick2"],
+                ["row3stick1", "row3stick2", "row3stick3"]]
+
+// player 1 to start the game
